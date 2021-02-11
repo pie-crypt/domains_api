09b38fb36892a9ffcaaaf50633aadfda79967fa4
pie-crypt <houssem.hosni172@gmail.com>
2021-02-11 23:57:42 +0100

    commit

--- models/domain.py
+++ models/domain.py
@@ -4,17 +4,26 @@ class DomainModel(db.Model):
     __tablename__ = 'domains'
     #id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, primary_key=True)
-    creation_date = db.Column(db.String)
-    expiration_date = db.Column(db.String)
-    updated_date = db.Column(db.String)
+    creation_date = db.Column(db.String, primary_key=True)
+    expiration_date = db.Column(db.String, primary_key=True)
+    updated_date = db.Column(db.String, primary_key=True)
     registrar = db.Column(db.String)
     whois_server = db.Column(db.String)
     name_servers = db.Column(db.String)
     status = db.Column(db.String)
     country = db.Column(db.String)
+    a = db.Column(db.String)
+    aaaa = db.Column(db.String)
+    ns = db.Column(db.String)
+    cname = db.Column(db.String)
+    mx = db.Column(db.String)
+    txt = db.Column(db.String)
+    soa = db.Column(db.String)
+    dnskey = db.Column(db.String)
 
-    def __init__(self, name, creation_date, expiration_date, updated_date, registrar,
-    whois_server, name_servers, status, country):
+    def __init__(self, name, creation_date, expiration_date, updated_date, 
+                registrar, whois_server, name_servers, status, country, a, 
+                aaaa, ns, cname, mx, txt, soa, dnskey):
         self.name = name
         self.creation_date = creation_date
         self.expiration_date = expiration_date
@@ -24,6 +33,14 @@ class DomainModel(db.Model):
         self.name_servers = name_servers
         self.status = status
         self.country = country
+        self.a = a
+        self.aaaa = aaaa
+        self.ns = ns
+        self.cname = cname
+        self.mx = mx
+        self.txt = txt
+        self.soa = soa
+        self.dnskey = dnskey
 
 
     def json(self):
@@ -31,7 +48,9 @@ class DomainModel(db.Model):
         , "expiration_date":self.expiration_date, "updated_date":self.updated_date
         , "registrar":self.registrar, "whois_server":self.whois_server
         , "name_servers":self.name_servers, "status":self.status
-        , "country":self.country}
+        , "country":self.country, "A": self.a, "AAAA": self.aaaa
+        , "NS": self.ns, "CNAME": self.cname, "MX": self.mx, "TXT": self.txt
+        , "SOA": self.soa, "DNSKEY": self.dnskey}
 
     @classmethod
     def get_by_name(cls, name):
